6294be636fccdab32df5ae4a973402b1c2b432ac
dburihabwa <dorian.burihabwa@gmail.com>
2012-10-26 10:35:32 +0200

    [MOD] Handling of the mouse click implemented

--- SokobanLogique/DirectionButtonEventHandler.cpp
+++ SokobanLogique/DirectionButtonEventHandler.cpp
@@ -1,6 +1,7 @@
 #include "DirectionButtonEventHandler.h"
 
 
+
 Sokoban::DirectionButtonEventHandler::DirectionButtonEventHandler(Sokoban::Direction direction)
 {
     this->_direction = direction;
@@ -13,4 +14,26 @@ Sokoban::DirectionButtonEventHandler::~DirectionButtonEventHandler(void)
 
 void Sokoban::DirectionButtonEventHandler::operator()(Node* node, NodeVisitor* nv)
 {
+	std::list<ref_ptr<GUIEventAdapter>> events;
+	EventVisitor* ev = dynamic_cast<EventVisitor*>(nv);
+	if(ev) {
+		events = ev->getEvents(); // Récupère les "évènements" dans la liste
+		ref_ptr<GUIEventAdapter> ea = events.back(); // Récupère le dernier élément de la liste
+		events.pop_back(); // Supprime l'élément récupéré de la liste
+		switch(ea->getEventType())
+		{
+		case GUIEventAdapter::PUSH: // Button Click
+			switch(ea->getKey()){
+            case GUIEventAdapter::LEFT_MOUSE_BUTTON:
+                Sokoban::Board::getInstance().movePlayer(this->_direction);
+                break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	traverse(node,nv);
 }
--- SokobanLogique/DirectionButtonEventHandler.h
+++ SokobanLogique/DirectionButtonEventHandler.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <list>
 
 #include <osg/NodeCallback>
+#include <osgGA/GUIEventAdapter>
+#include <osgGA/EventVisitor>
 
+#include "Board.h"
 #include "DirectionButton.h"
+
+using namespace osgGA;
+using namespace osg;
 
 namespace Sokoban {
     class DirectionButtonEventHandler : public osg::NodeCallback
