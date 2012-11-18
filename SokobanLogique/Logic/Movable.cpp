06de4b817d7a230e7b8c53141fe1525305aad1b4
Antoine Aflalo <antoineaf@gmail.com>
2012-11-18 15:03:18 +0100

    [MOD] Use the MoveAdapter in the Movable.

--- SokobanLogique/Logic/Movable.cpp
+++ SokobanLogique/Logic/Movable.cpp
@@ -4,17 +4,11 @@ void Sokoban::Movable::move(Direction dir) {
 	int newX = _x+dir.getX();
 	int newY = _y+dir.getY();
 	Board::getInstance().swapMovable(_x,_y,newX,newY);
-	_aniUpdater->setMovement(getPosition(),Vec3(newX,newY,0));
+	_moveAdapter->move(getPosition(),Vec3(newX,newY,0), dir);
 	this->_x = newX;
 	this->_y = newY;
 }
 
 ref_ptr<Sokoban::Case> Sokoban::Movable::getCase(Direction dir) const {
 	return Board::getInstance().getCase(this->_x+dir.getX(),this->_y+dir.getY());
-}
-ref_ptr<Node> Sokoban::Movable::createNode() {
-	_graphNode = Case::createNode();
-	_aniUpdater = new AnimationUpdater();
-	_graphNode->setUpdateCallback(_aniUpdater);
-	return _graphNode;
 }
