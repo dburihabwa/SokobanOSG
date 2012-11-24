0400b6859b5777e67f6c007b4cb070661f44ef83
dburihabwa <dorian.burihabwa@gmail.com>
2012-11-24 21:43:12 +0100

    [DOC] Added comments in GUIButton

--- SokobanLogique/GUI/GUIButton.h
+++ SokobanLogique/GUI/GUIButton.h
@@ -7,7 +7,9 @@ namespace Sokoban {
 		public Sokoban::Case
 	{
 	public:
+		/// GUIButton default constructor
 		GUIButton(int x, int y, int z);
+		/// Preforms the expected behaviour on a click event
 		virtual bool onClick() = 0;
 	};
 };
