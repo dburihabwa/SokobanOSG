083977decb339fd243d5db2baaf952a34f269c9a
Antoine Aflalo <antoineaf@gmail.com>
2012-11-30 11:53:30 +0100

    [ADD] Adding display of the score in the textPanel. See #1

--- SokobanLogique/GUI/TextPanel.h
+++ SokobanLogique/GUI/TextPanel.h
@@ -18,11 +18,19 @@ namespace Sokoban {
 		void init();
 		osg::ref_ptr<osg::Camera> _camera;
 		osg::ref_ptr<osg::Group> _textGroup;
+		osg::ref_ptr<osgText::Text> _movementScore;
+		osg::ref_ptr<osgText::Text> _boxScore;
 		unsigned int _nbTxt;
 		unsigned int _maxHeight;
+		unsigned int _mvntScore;
+		unsigned int _bScore;
 		static osg::ref_ptr<osgText::Font> _font;
 		void updatePosition();
 		std::map<std::string,osg::ref_ptr<osg::Geode>> _textCache;
 		osg::ref_ptr<osg::Geode> getOrCreateGeode(std::string text, osg::Vec4 color);
+		osg::ref_ptr<osgText::Text> generateText(std::string text, osg::Vec4 color);
+		osg::ref_ptr<osg::Geode> generateGeode(osg::ref_ptr<osgText::Text>);
+		static std::string _playerString;
+		static std::string _boxString;
 	};
 }
