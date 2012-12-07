e3eb6a1677519a48667ce3b4ec53f58a83dd8655
Antoine Aflalo <antoineaf@gmail.com>
2012-12-07 12:35:12 +0100

    Revert "[MOD] Set emission of normal graphical nodes"
    This reverts commit 07d88f856f42752545bd85c168d4f0e66ce9cdbd.

--- SokobanLogique/Factories/NodeFactory.cpp
+++ SokobanLogique/Factories/NodeFactory.cpp
@@ -197,9 +197,10 @@ osg::ref_ptr<osg::Geode> Sokoban::NodeFactory::getOrCreateGeode(Type element) {
 	}
 	if (element != GROUND)
 		noeudGeo->addDrawable(shape);
+
 	// create a simple material
 	osg::Material *material = new osg::Material();
-	material->setEmission(osg::Material::FRONT, osg::Vec4(0.3, 0.3, 0.3, 0.5));
+	material->setEmission(osg::Material::FRONT, osg::Vec4(0.8, 0.8, 0.8, 1.0));
 	// create a texture
 	// load image for texture
 	osg::ref_ptr<osg::Image> image = osgDB::readImageFile(textureImage);
